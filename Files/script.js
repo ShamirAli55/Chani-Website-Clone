53288146f98d2e644398529a82c2039b7f1a9fe9
ShamirAli55 <shamirali9779@gmail.com>
2025-07-24 04:06:01 -0700

    Add north_star.webp image asset to the project

--- Files/script.js
+++ Files/script.js
@@ -1,7 +1,23 @@
- var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-      },
-    });
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+let lip = document.querySelector("#lip");
+window.addEventListener("scroll",()=>
+{
+  let scrollY = window.scrollY;
+  let triggerPoint =lip.offsetTop - window.innerHeight;
+
+  if (scrollY >= triggerPoint) {
+    let moveAmount = (scrollY );
+    lip.style.height = moveAmount * 0.3 + "px";
+  }
+   else 
+  {
+    lip.style.Height = "30vw";
+  }
+})
