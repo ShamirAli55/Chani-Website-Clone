c2a16c1ffedec0c5a2f93604d72fa616bd57044d
ShamirAli55 <shamirali9779@gmail.com>
2025-07-25 06:40:45 -0700

    Refactor code structure for improved readability and maintainability

--- Files/script.js
+++ Files/script.js
@@ -1,11 +1,10 @@
-var swiper = new Swiper('.swiper', {
+var swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  }
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
-
 
 let lip = document.querySelector("#lip");
 window.addEventListener("scroll", () => {
@@ -15,12 +14,10 @@ window.addEventListener("scroll", () => {
   if (scrollY > triggerPoint) {
     let moveAmount = scrollY;
     lip.style.height = -10 + moveAmount * 0.17 + "px";
-  } 
-  else {
+  } else {
     lip.style.Height = "30vw";
   }
 });
-
 
 let tiger = document.querySelector("#tiger");
 let astro = document.querySelector("#astro");
@@ -31,19 +28,17 @@ window.addEventListener("scroll", () => {
 
   if (scrollY >= triggerPoint) {
     let offset = scrollY - triggerPoint;
-    
-    let tigerScale = Math.min( offset * 0.0005, 2);  
-    let astroScale = Math.max( offset * 0.0005, 1.5); 
+
+    let tigerScale = Math.min(offset * 0.0005, 2);
+    let astroScale = Math.max(offset * 0.0005, 1.5);
 
     tiger.style.transform = `scale(${tigerScale})`;
     astro.style.transform = `scale(${astroScale})`;
-  } 
-  else {
+  } else {
     tiger.style.transform = `scale(0.3)`;
     astro.style.transform = `scale(0.8)`;
   }
 });
-
 
 let chain = document.querySelector("#chain");
 
@@ -58,23 +53,18 @@ window.addEventListener("scroll", () => {
   }
 });
 
-
-
 let diamond = document.querySelector("#diamond");
 window.addEventListener("scroll", () => {
   let scrollY = window.scrollY;
   let triggerPoint = diamond.offsetTop - window.innerHeight;
 
   if (scrollY > triggerPoint) {
-    let moveAmount = scrollY;;
-    diamond.style.bottom =  -5 + moveAmount * 0.161 + "px";
-  } 
-  else {
+    let moveAmount = scrollY;
+    diamond.style.bottom = -5 + moveAmount * 0.161 + "px";
+  } else {
     diamond.style.bottom = "60%";
   }
 });
-
-
 
 let hand = document.querySelector("#hand");
 window.addEventListener("scroll", () => {
@@ -83,16 +73,13 @@ window.addEventListener("scroll", () => {
 
   if (scrollY > triggerPoint) {
     let moveAmount = scrollY;
-    hand.style.right =  -550 + moveAmount * 0.1 + "px";
-  } 
-  else {
-    hand.style.right= "-10%";
+    hand.style.right = -550 + moveAmount * 0.1 + "px";
+  } else {
+    hand.style.right = "-10%";
   }
 });
 
-
-
-let chani= document.querySelector("#chani");
+let chani = document.querySelector("#chani");
 
 window.addEventListener("scroll", () => {
   let scrollY = window.scrollY;
@@ -105,19 +92,15 @@ window.addEventListener("scroll", () => {
   }
 });
 
-
-
-let chicken= document.querySelector("#chicken");
+let chicken = document.querySelector("#chicken");
 window.addEventListener("scroll", () => {
   let scrollY = window.scrollY;
   let triggerPoint = chicken.offsetTop - window.innerHeight;
 
   if (scrollY > triggerPoint) {
-    let moveAmount = scrollY;;
-    chicken.style.bottom =  -5 + moveAmount * 0.16 + "px";
-  } 
-  else {
+    let moveAmount = scrollY;
+    chicken.style.bottom = -5 + moveAmount * 0.16 + "px";
+  } else {
     chicken.style.left = "10%";
   }
 });
-
