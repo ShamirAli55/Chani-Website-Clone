7da32724a3182ed76696421458b828672fd4b0fb
ShamirAli55 <shamirali9779@gmail.com>
2025-07-25 03:07:46 -0700

    Add speed limit asset in WEBP format

--- Files/script.js
+++ Files/script.js
@@ -7,38 +7,44 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 let lip = document.querySelector("#lip");
-window.addEventListener("scroll",()=>
-{
+window.addEventListener("scroll", () => {
   let scrollY = window.scrollY;
-  let triggerPoint =lip.offsetTop - window.innerHeight;
+  let triggerPoint = lip.offsetTop - window.innerHeight;
 
   if (scrollY >= triggerPoint) {
-    let moveAmount = (scrollY );
+    let moveAmount = scrollY;
     lip.style.height = -10 + moveAmount * 0.17 + "px";
-  }
-   else 
-  {
+  } else {
     lip.style.Height = "30vw";
   }
-})
+});
 
 let tiger = document.querySelector("#tiger");
 let astro = document.querySelector("#astro");
 
-window.addEventListener("scroll",()=>
-{
-let scrollY = window.scrollY;
-  let triggerPoint =tiger.offsetTop - window.innerHeight;
+window.addEventListener("scroll", () => {
+  let scrollY = window.scrollY;
+  let triggerPoint = tiger.offsetTop - window.innerHeight;
 
-  if (scrollY >= triggerPoint) 
-  {
-    let moveAmount = (scrollY );
+  if (scrollY >= triggerPoint) {
+    let moveAmount = scrollY;
     tiger.style.height = moveAmount * 0.1 + "px";
     astro.style.height = moveAmount * 0.1 + "px";
-  }
-   else 
-  {
+  } else {
     tiger.style.height = "270px";
-    astro.style.height= "270px";
+    astro.style.height = "270px";
   }
-})
+});
+
+let chain = document.querySelector("#chain");
+
+window.addEventListener("scroll", () => {
+  let scrollY = window.scrollY;
+  let triggerPoint = chain.offsetTop - window.innerHeight;
+
+  if (scrollY >= triggerPoint) {
+    let tilt = Math.sin(scrollY * 0.01) * 6;
+
+    chain.style.transform = `rotateZ(${tilt}deg)`;
+  }
+});
